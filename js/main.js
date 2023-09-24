a3fc7fb9987e51348636a4b28d180cb1b410c9f8
Lucas Mendez <mendezlucas777@gmail.com>
2023-09-24 16:13:05 -0300

    Creacion del main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Productos del Ecommerce
+// El siguiente array va a tener todos los productos disponibles en el Ecommerce,
+// este va a estar dividido en categorias que luego se van a ir completando
+
+const productos = [
+    // Teclados
+    {
+        id: "teclado-1",
+        nombre: "Teclado N1",
+        imagen: "ruta de la imagen",
+        categoria: "Teclados",
+        precio: 6700
+    },
+    {
+        id: "teclado-2",
+        nombre: "Teclado N2",
+        imagen: "ruta de la imagen",
+        categoria: "Teclados",
+        precio: 7700
+    },
+    {
+        id: "teclado-3",
+        nombre: "Teclado N3",
+        imagen: "ruta de la imagen",
+        categoria: "Teclados",
+        precio: 8700
+    }
+
+    // Aca se completaran las otras categorias
+]
