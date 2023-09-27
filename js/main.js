f9c36398acd27b5f1e57a10e4d6d8b52418d91ca
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-09-27 16:23:02 -0300

    Organizamos las imágenes, según su categoría
    Parte 1

--- js/main.js
+++ js/main.js
@@ -21,14 +21,14 @@ const productos = [
     },
     // Mouse
     {
-        id: "mouse-1",
+        id: "mouse_1",
         nombre: "Mouse Glorious Model O Minus (Glossy White)",
         imagen: "/imagenes/perifericos/mouse_1.webp",
         categoria: "Mouse",
         precio: 21990
     },
     {
-        id: "mouse-2",
+        id: "mouse_2",
         nombre: "Mouse Logitech G203 Lightsync RGB Black",
         imagen: "/imagenes/perifericos/mouse_2.webp",
         categoria: "Mouse",
@@ -41,9 +41,82 @@ const productos = [
         imagen: "/imagenes/perifericos/auricular_1.webp",
         categoria: "Auriculares",
         precio: 20000
-    }
-    
-
-
+    },
+    // Sillas
+    {
+        id: "silla_1",
+        nombre: "Silla Gamer Checkpoint FIFA MT-2000 Qatar",
+        imagen: "/imagenes/Sillas/silla_1.webp",
+        categoria: "Sillas",
+        precio: 175000
+    },
+    {
+        id: "silla_2",
+        nombre: "Silla Gamer Vertagear Racing Series PL-4500 Crystals from Swarovski",
+        imagen: "/imagenes/Sillas/silla_2.webp",
+        categoria: "Sillas",
+        precio:  533350
+    },
+    // Coolers
+    {
+        id: "cooler_1",
+        nombre: "Cooler Fan Cooler Master MF120 S3",
+        imagen: "/imagenes/Componentes/cooler_1.webp",
+        categoria: "Coolers",
+        precio: 8900
+    },
+    // Discos
+    {
+        id: "disco_1",
+        nombre: "Disco Sólido SSD Kingston 480GB A400 500MB/s",
+        imagen: "/imagenes/Componentes/disco_1.webp",
+        categoria: "Discos",
+        precio: 21700
+    },
+    // Fuentes
+    {
+        id: "fuente_1",
+        nombre: "Fuente ASUS ROG STRIX 1000W 80 Plus Gold Full Modular 1000G",
+        imagen: "/imagenes/Componentes/fuente_1.webp",
+        categoria: "Fuentes",
+        precio: 140000
+    },
+    {
+        id: "fuente_2",
+        nombre: "Fuente Gigabyte 650W 80 Plus Bronze P650B",
+        imagen: "/imagenes/Componentes/fuente_2.webp",
+        categoria: "Fuentes",
+        precio: 57900
+    },
+    //Memorias
+    {
+        id: "memoria_1",
+        nombre: "Memoria Crucial DDR4 8GB 2666MHz",
+        imagen: "/imagenes/Componentes/memoria_1.webp",
+        categoria: "Memorias",
+        precio: 20199
+    },
+    {
+        id: "memoria_2",
+        nombre: "Memoria Team DDR4 32GB 2666MHz T-Force Delta RGB",
+        imagen: "/imagenes/Componentes/memoria_2.webp",
+        categoria: "Memorias",
+        precio: 75000
+    },
+    // Motherboard
+    {
+        id: "placa_madre_1",
+        nombre: "Mother ASUS PRIME A320M-K AM4",
+        imagen: "/imagenes/Componentes/placa_madre_1.webp",
+        categoria: "Placas Madre",
+        precio:  77999
+    },
+    {
+        id: "placa_madre_2",
+        nombre: "Mother ASUS ROG STRIX B550-F GAMING WIFI II",
+        imagen: "/imagenes/Componentes/placa_madre_2.webp",
+        categoria: "Placas Madre",
+        precio:  190000
+    },
     // Aca se completaran las otras categorias
 ]
