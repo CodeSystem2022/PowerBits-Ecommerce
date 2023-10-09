c5d0d2778057803aa104d15bb73390013707cff3
Nico25Mac <112595518+Nico25Mac@users.noreply.github.com>
2023-10-08 21:19:57 -0300

    Update
    Actualizar número del botón carrito, creamos una función para actualizar el número de la cantidad de productos que hay en el carrito.
    También corregí algunos errores
    Actualice el logo de inicio y carrito

--- js/main.js
+++ js/main.js
@@ -8,15 +8,21 @@ const productos = [
     {
         id: "teclado_1",
         nombre: "Teclado Mecanico HyperX Alloy CORE RGB LA",
-        imagen: "imagenes/perifericos/teclado_1.webp",
-        categoria: "Teclados",
+        imagen: "/imagenes/perifericos/teclado_1.webp",
+        categoria: {
+            nombre: "Perifericos",
+            id: "Perifericos"
+        },
         precio: 33900
     },
     {
         id: "teclado_2",
         nombre: "Teclado Mecanico Redragon Dark Avenger K568 RGB Switch Red",
         imagen: "/imagenes/perifericos/teclado_2.webp",
-        categoria: "Teclados",
+        categoria: {
+            nombre: "Perifericos",
+            id: "Perifericos"
+        },
         precio: 31000
     },
     // Mouse
@@ -24,14 +30,20 @@ const productos = [
         id: "mouse_1",
         nombre: "Mouse Glorious Model O Minus (Glossy White)",
         imagen: "/imagenes/perifericos/mouse_1.webp",
-        categoria: "Mouse",
+        categoria: {
+            nombre: "Perifericos",
+            id: "Perifericos"
+        },
         precio: 21990
     },
     {
         id: "mouse_2",
         nombre: "Mouse Logitech G203 Lightsync RGB Black",
         imagen: "/imagenes/perifericos/mouse_2.webp",
-        categoria: "Mouse",
+        categoria: {
+            nombre: "Perifericos",
+            id: "Perifericos"
+        },
         precio: 20000
     },
     // Auriculares
@@ -39,7 +51,10 @@ const productos = [
         id: "auricular_1",
         nombre: "Auriculares HyperX Cloud Flight Black Wireless",
         imagen: "/imagenes/perifericos/auricular_1.webp",
-        categoria: "Auriculares",
+        categoria: {
+            nombre: "Perifericos",
+            id: "Perifericos"
+        },
         precio: 20000
     },
     // Sillas
@@ -47,14 +62,20 @@ const productos = [
         id: "silla_1",
         nombre: "Silla Gamer Checkpoint FIFA MT-2000 Qatar",
         imagen: "/imagenes/Sillas/silla_1.webp",
-        categoria: "Sillas",
+        categoria: {
+            nombre: "Sillas",
+            id: "Sillas"
+        },
         precio: 175000
     },
     {
         id: "silla_2",
         nombre: "Silla Gamer Vertagear Racing Series PL-4500 Crystals from Swarovski",
         imagen: "/imagenes/Sillas/silla_2.webp",
-        categoria: "Sillas",
+        categoria: {
+            nombre: "Sillas",
+            id: "Sillas"
+        },
         precio:  533350
     },
     // Coolers
@@ -62,7 +83,10 @@ const productos = [
         id: "cooler_1",
         nombre: "Cooler Fan Cooler Master MF120 S3",
         imagen: "/imagenes/Componentes/cooler_1.webp",
-        categoria: "Coolers",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 8900
     },
     // Discos
@@ -70,7 +94,10 @@ const productos = [
         id: "disco_1",
         nombre: "Disco Sólido SSD Kingston 480GB A400 500MB/s",
         imagen: "/imagenes/Componentes/disco_1.webp",
-        categoria: "Discos",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 21700
     },
     // Fuentes
@@ -78,14 +105,20 @@ const productos = [
         id: "fuente_1",
         nombre: "Fuente ASUS ROG STRIX 1000W 80 Plus Gold Full Modular 1000G",
         imagen: "/imagenes/Componentes/fuente_1.webp",
-        categoria: "Fuentes",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 140000
     },
     {
         id: "fuente_2",
         nombre: "Fuente Gigabyte 650W 80 Plus Bronze P650B",
         imagen: "/imagenes/Componentes/fuente_2.webp",
-        categoria: "Fuentes",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 57900
     },
     //Memorias
@@ -93,14 +126,20 @@ const productos = [
         id: "memoria_1",
         nombre: "Memoria Crucial DDR4 8GB 2666MHz",
         imagen: "/imagenes/Componentes/memoria_1.webp",
-        categoria: "Memorias",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 20199
     },
     {
         id: "memoria_2",
         nombre: "Memoria Team DDR4 32GB 2666MHz T-Force Delta RGB",
         imagen: "/imagenes/Componentes/memoria_2.webp",
-        categoria: "Memorias",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio: 75000
     },
     // Motherboard
@@ -108,14 +147,20 @@ const productos = [
         id: "placa_madre_1",
         nombre: "Mother ASUS PRIME A320M-K AM4",
         imagen: "/imagenes/Componentes/placa_madre_1.webp",
-        categoria: "Placas Madre",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:  77999
     },
     {
         id: "placa_madre_2",
         nombre: "Mother ASUS ROG STRIX B550-F GAMING WIFI II",
         imagen: "/imagenes/Componentes/placa_madre_2.webp",
-        categoria: "Placas Madre",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:  190000
     },
     // Placas de Video
@@ -123,28 +168,40 @@ const productos = [
         id: "placa_video_1",
         nombre: "Placa de Video Asrock Radeon RX 5500 XT 8GB GDDR6 CHALLENGER D OC",
         imagen: "/imagenes/Componentes/placa_video_1.webp",
-        categoria: "Placas de Video",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:  204200
     },
     {
         id: "placa_video_2",
         nombre: "Placa de Video ASUS GeForce GTX 1650 4GB GDDR6 Phoenix EVO OC",
         imagen: "/imagenes/Componentes/placa_video_2.webp",
-        categoria: "Placas de Video",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:   159290
     },
     {
         id: "placa_video_3",
         nombre: "Placa de Video XFX Radeon RX 7900 XTX 24GB GDDR6 SPEEDSTER MERC 310  ",
         imagen: "/imagenes/Componentes/placa_video_3.webp",
-        categoria: "Placas de Video",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:   1335900
     },
     {
         id: "placa_video_4",
         nombre: "placa de video ZOTAC Gaming GeForce RTX 4090 AMP Extreme AIRO 24GB  ",
         imagen: "/imagenes/Componentes/placa_video_4.webp",
-        categoria: "Placas de Video",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:   1240000
     },
     // Procesadores
@@ -152,37 +209,52 @@ const productos = [
         id: "procesador_1",
         nombre: "Procesador AMD Ryzen 7 5800X 4.7GHz Turbo AM4",
         imagen: "/imagenes/Componentes/procesador_1.webp",
-        categoria: "Procesadores",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:   550000
     },
     {
         id: "procesador_2",
         nombre: "Procesador Intel Core i9 13900KF 5.8GHz Turbo ",
         imagen: "/imagenes/Componentes/procesador_2.webp",
-        categoria: "Procesadores",
+        categoria: {
+            nombre: "Componentes",
+            id: "Componentes"
+        },
         precio:   554250
     },
     // Monitores
     {
         id: "monitor_1",
         nombre: "Monitor Gamer ViewSonic 24.5 XG251G 360Hz IPS",
-        imagen: "/imagenes/Monitores/monitor_1",
-        categoria: "Monitores",
+        imagen: "/imagenes/Monitores/monitor_1.webp",
+        categoria: {
+            nombre: "Monitores",
+            id: "Monitores"
+        },
         precio:   600000
     },
     {
         id: "monitor_2",
         nombre: "Monitor Philips 19 HDMI VGA",
-        imagen: "/imagenes/Monitores/monitor_2",
-        categoria: "Monitores",
+        imagen: "/imagenes/Monitores/monitor_2.webp",
+        categoria: {
+            nombre: "Monitores",
+            id: "Monitores"
+        },
         precio:    60000
     },
     // Notebooks
     {
         id: "notekook_1",
         nombre: "Notebook Gamer Acer Nitro 5  FHD Core I5 11400H 8GB 512GB SSD NVMe RTX 3050 W11 144hz",
-        imagen: "/imagenes/Notebooks/notebook_1",
-        categoria: "Notebooks",
+        imagen: "/imagenes/Notebooks/notebook_1.webp",
+        categoria: {
+            nombre: "Notebooks",
+            id: "Notebooks"
+        },
         precio:    750000
     },
 
@@ -190,9 +262,10 @@ const productos = [
 ]
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".botone-categoria");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
 
 function cargarProdutos(productosElegidos) {
@@ -208,11 +281,13 @@ function cargarProdutos(productosElegidos) {
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.nombre}</h3>
                 <p class="producto-precio">$${producto.precio}</p>
-                <button class="producto-agregar" id="${producto.id}"></button>
+                <button class="producto-agregar" id="${producto.id}">AGREGAR</button>
             </div>
         `;
         contenedorProductos.append(div);
     })
+
+    actualizarBotonesAgregar();
 }
 
 cargarProdutos(productos);
@@ -224,9 +299,9 @@ botonesCategorias.forEach(boton => {
         e.currentTarget.classList.add("active");
 
         if(e.currentTarget.id != "todos") {
-            const productoCategorias = productos.find(producto => producto.categoria.id === e.currentTargetid);
-            console.log(productoCategorias)
-            tituloPrincipal.innerText = productoCategorias.categoria.nombre;
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            console.log(productoCategoria);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
             const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
             cargarProdutos(productosBoton);
 
@@ -250,14 +325,22 @@ const productosEnCarrito = [];
 function agregarAlCarrito(e) {
 
     const IdBoton = e.currentTarget.id;
-    const productoAgregado = productos.find(producto => producto.id === idBoton);
+    const productoAgregado = productos.find(producto => producto.id === IdBoton);
 
-    if(productosEnCarrito.some (producto => producto.id === idBoton)) {
-      const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    if(productosEnCarrito.some (producto => producto.id === IdBoton)) {
+      const index = productosEnCarrito.findIndex(producto => producto.id === IdBoton);
       productosEnCarrito[index].cantidad++;
 
     } else {
         productoAgregado.cantidad = 1;
         productosEnCarrito.push(productoAgregado);
     }    
+    actualizarNumerito();
+    
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito(){
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
 }
